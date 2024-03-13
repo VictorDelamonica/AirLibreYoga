8d05b7276b24b5b3a6ecde43bb502f6f563293f6
Victor Delamonica <victor.delamonica@epitech.eu>
2024-03-13 12:45:26 +0000

    Add Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,22 @@ import 'package:air_libre_yoga/views/empty_view.dart';
 import 'package:air_libre_yoga/views/people_view.dart';
 import 'package:air_libre_yoga/views/session_view.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
-
 Future<void> main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    FlutterError.onError = (errorDetails) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+    };
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+
   runApp(const MyApp());
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 }
 
 class MyApp extends StatelessWidget {
