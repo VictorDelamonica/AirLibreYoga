84ee31576dd9a7785ebd4324b03e0a57fb159f23
Victor Delamonica <victor.delamonica@icloud.com>
2024-03-27 12:24:40 +0000

    Fix job

--- .space.kts
+++ .space.kts
@@ -1,4 +1,11 @@
 job("Deploy to space") {
+  container(displayName = "Say Hello", image = "ubuntu") {
+        shellScript {
+            content = """
+                echo "I can run shellScript only once!"
+            """
+        }
+    }
     container(displayName = "Api", image = "amazoncorretto:17-alpine") {
         kotlinScript { api ->
             api.space().projects.automation.deployments.start(
